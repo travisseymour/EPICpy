e86ae648cb3655d731314ee2dc337ff0f52c195c
Travis L. Seymour <nogard@ucsc.edu>
2025-08-16 00:18:09 -0700

    fixed update notice

--- epicpy/constants/version.py
+++ epicpy/constants/version.py
@@ -17,6 +17,6 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '2024.8.15.dev1'
-__version__ = '2024.8.15.dev1'
-__version_tuple__ = version_tuple = (2024, 8, 15, 'dev1')
+version = '2024.8.15.dev2'
+__version__ = '2024.8.15.dev2'
+__version_tuple__ = version_tuple = (2024, 8, 15, 'dev2')
